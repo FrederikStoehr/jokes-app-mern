94a06d52520ba69c7dffec48ae71c929c75de121
FrederikStoehr <frederik.stoehr@gmail.com>
2025-08-17 23:20:45 +0200

    v11

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <h1>Programming Jokes w/ ECS NEW</h1>
+      <h1>Programming Jokes w/ ECS NEWWW</h1>
       <hr />
       <JokeList jokes={jokes} />
     </div>
